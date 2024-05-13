513d29e540736d8d2643a2c18aaab56389ee1a35
VascoCosta <vascocosta03@gmail.com>
2024-05-13 09:41:26 +0100

    delete

--- assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.fa-heart').on('click', function() {
-        $(this).toggleClass('text-muted text-danger');
-    });
-
-    $('.card-produto .card-body').on('click', function() {
-        window.location.href = 'produto.html';
-    })
-});
